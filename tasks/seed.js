d28a0d6ece2a3d7ea9dbd5031c369be8894f2e82
zhouhuihai@yahoo.com <zhouhuihai@yahoo.com>
2019-05-12 17:22:47 -0400

    can now seed events

--- tasks/seed.js
+++ tasks/seed.js
@@ -11,7 +11,7 @@ seeder.connect('mongodb://localhost/tutoringwebsite', function() {
  
   // Clear specified collections
   // **** this clears the current userdb, so seeding multiple times gives you the same db.
-  seeder.clearModels(['user'], function() {
+  seeder.clearModels(['User'], function() {
  
     // Callback to populate DB once collections have been cleared
     seeder.populateModels(data, function() {
@@ -24,12 +24,12 @@ seeder.connect('mongodb://localhost/tutoringwebsite', function() {
 // Data array containing seed data - documents organized by Model
 var data = [
     {
-        'model': 'user',
+        'model': 'User',
         'documents': [
           {
             email: "seed1@gmail.com",
             username: "seed1",
-            hashedPassword: "$2a$10$/8p/dzNqYbUkGhBdGhNMve8A4KSPbYt1CMbL4kqXTT3EG1aj0kKSq", // I think it's "asd123" ? could be wrong
+            hashedPassword: "$2a$10$/8p/dzNqYbUkGhBdGhNMve8A4KSPbYt1CMbL4kqXTT3EG1aj0kKSq", // it's "asd123"
             createdAt: '2019-05-10 19:04:56.260',
             updatedAt: '2019-05-10 19:04:56.260',
             profile: {
@@ -44,12 +44,12 @@ var data = [
         ]
     },
     {
-      'model': 'user',
+      'model': 'User',
       'documents': [
         {
           email: "seed2@gmail.com",
           username: "seed2",
-          hashedPassword: "$2a$10$/8p/dzNqYbUkGhBdGhNMve8A4KSPbYt1CMbL4kqXTT3EG1aj0kKSq", // I think it's "asd123" ? could be wrong
+          hashedPassword: "$2a$10$/8p/dzNqYbUkGhBdGhNMve8A4KSPbYt1CMbL4kqXTT3EG1aj0kKSq", // It's "asd123"
           createdAt: '2019-05-10 19:04:56.260',
           updatedAt: '2019-05-10 19:04:56.260',
           profile: {
@@ -64,12 +64,12 @@ var data = [
       ]
     },
     {
-      'model': 'user',
+      'model': 'User',
       'documents': [
         {
           email: "seed3@gmail.com", // this one does not have a profile. But can be added after logging in
           username: "seed3",
-          hashedPassword: "$2a$10$/8p/dzNqYbUkGhBdGhNMve8A4KSPbYt1CMbL4kqXTT3EG1aj0kKSq", // I think it's "asd123" ? could be wrong
+          hashedPassword: "$2a$10$/8p/dzNqYbUkGhBdGhNMve8A4KSPbYt1CMbL4kqXTT3EG1aj0kKSq", // It's "asd123"
           createdAt: '2019-05-10 19:04:56.260',
           updatedAt: '2019-05-10 19:04:56.260'
         }
